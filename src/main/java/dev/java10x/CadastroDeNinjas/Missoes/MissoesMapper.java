64a418e032dd9a433a86dc958139b05fb4109f3a
Matheus Yuri Silva <matheusyurirs@gmail.com>
2025-03-28 17:06:19 -0300

    fix: Tira a responsabilidade do Model Missoes, e passa para dentro do meu DTO usando streams e Mapper

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
@@ -1,4 +1,27 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNinja(missoesDTO.getNinja());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setTitulo(missoesDTO.getTitulo());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel){
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNinja(missoesModel.getNinja());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setTitulo(missoesModel.getTitulo());
+
+        return missoesDTO;
+    }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,40 +1,69 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaDTO;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissoesService {
     private MissoesRepository missoesRepository;
+    private MissoesMapper missoesMapper;
 
-    public MissoesService(MissoesRepository missoesRepository) {
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
         this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
     }
 
     //Listar todas as missões
-    public List<MissoesModel> listarMissoes(){
-        return missoesRepository.findAll();
+    public List<MissoesDTO> listarMissoes() {
+        List<MissoesModel> missoes = missoesRepository.findAll();
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
     }
 
-    //Criar missao
+    public MissoesDTO listarMissoesPorId(Long id) {
+        Optional<MissoesModel> missoesPorId = missoesRepository.findById(id);
+        return missoesPorId.map(missoesMapper::map).orElse(null);
+    }
 
-    public MissoesModel criarMissoes(MissoesModel missoes){
-        return missoesRepository.save(missoes);
+    public MissoesDTO criarMissoes(MissoesDTO missoesDTO) {
+        MissoesModel missoes = missoesMapper.map(missoesDTO);
+        missoes = missoesRepository.save(missoes);
+        return missoesMapper.map(missoes);
     }
 
     //Deletar Missoes
 
-    public void deletarMissoesPorId(Long id){
+    public void deletarMissoesPorId(Long id) {
         missoesRepository.deleteById(id);
     }
 
     //Alterar missoes
-    public MissoesModel atualizarMissao(Long id, MissoesModel missoesAtualizada){
-        if(missoesRepository.existsById(id)){
-            missoesAtualizada.setId(id);
-            return missoesRepository.save(missoesAtualizada);
+    public MissoesDTO atualizarMissao(Long id, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missoesModelOptional = missoesRepository.findById(id);
+        if (missoesModelOptional.isPresent()) {
+            MissoesModel missoesExistem = missoesModelOptional.get();
+
+            if (missoesDTO.getNinja() != null ) {
+                missoesExistem.setNinja(missoesDTO.getNinja());
+            }
+
+            if(missoesDTO.getTitulo() != null){
+                missoesExistem.setTitulo(missoesDTO.getTitulo());
+            }
+
+            if(missoesDTO.getDificuldade() != null){
+                missoesExistem.setDificuldade(missoesDTO.getDificuldade());
+            }
+
+
+            MissoesModel missoesSalvo = missoesRepository.save(missoesExistem);
+            return missoesMapper.map(missoesSalvo);
         }
         return null;
     }
