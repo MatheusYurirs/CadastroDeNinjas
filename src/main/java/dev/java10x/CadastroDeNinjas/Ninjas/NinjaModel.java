83739739512ea351d34e1b2ca9a241510beb3084
Matheus Yuri Silva <matheusyurirs@gmail.com>
2025-03-31 11:09:57 -0300

    refactor: Inclui a anotation @ToString nos models de Ninja e Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missoes")
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
